f3899396a1b71c49d6f5c8cccc36bf80e9246a8d
Juri Kiin <cornfield24@gmail.com>
2018-03-01 11:02:19 -0500

    - Editing Camera projections

--- C10_MeshClass/AppClass.cpp
+++ C10_MeshClass/AppClass.cpp
@@ -2,8 +2,12 @@
 void Application::InitVariables(void)
 {
 	//Make MyMesh object
-	cube = new MyMesh();
-	cube->GenerateCube(1.0f, C_CYAN);
+	m_pMesh = new MyMesh();
+	m_pMesh->GenerateCube(1.0f, C_CYAN);
+
+	m_pMesh1 = new MyMesh();
+	m_pMesh1->GenerateCube(1.0f, C_CYAN);
+
 }
 void Application::Update(void)
 {
@@ -22,9 +26,30 @@ void Application::Display(void)
 	ClearScreen();
 
 	static float fPos = 0.0f;
+	float fovy = 45.0f;
+	float ratio = m_pSystem->GetWindowWidth() / static_cast<float>(m_pSystem->GetWindowHeight());
+	float fNear = 0.01f;
+	float fFar = 1000.0f;
 
-	m_m4cube = glm::translate(vector3(fPos,0.0f,0.0f));
+	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
+	m4Projection = glm::perspective(fovy, ratio, fNear, fFar);
+	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
 
+	vector3 v3Position(0, 0, 15);
+	vector3 v3Target(0, 0, 14);
+	vector3 v3Up(0, 1, 0);
+
+	m4View = glm::lookAt(v3Position, v3Target, v3Up);
+
+	m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_qArcBall));
+	m_pMesh1->Render(m4Projection, m4View, glm::translate(vector3(3.0f, 0.0f, 0.0f)));
+	//Need center, eye, and up
+
+
+
+
+	//SPACE INVADER EXERCISE
+	/*
 	//Position the cubes
 
 	//Center
@@ -55,11 +80,11 @@ void Application::Display(void)
 	cube->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(2.0f, 4.0f, 0.0f))*m_m4cube);
 
 	//X-1
-	cube->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-1.0f, -2.0f, 0.0f))*m_m4cube);
-	cube->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-1.0f, 0.0f, 0.0f))*m_m4cube);
-	cube->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-1.0f, 1.0f, 0.0f))*m_m4cube);
-	cube->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-1.0f, 2.0f, 0.0f))*m_m4cube);
-	cube->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-1.0f, 3.0f, 0.0f))*m_m4cube);
+	//cube->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-1.0f, -2.0f, 0.0f))*m_m4cube);
+	//cube->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-1.0f, 0.0f, 0.0f))*m_m4cube);
+	//cube->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-1.0f, 1.0f, 0.0f))*m_m4cube);
+	//cube->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-1.0f, 2.0f, 0.0f))*m_m4cube);
+	//cube->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-1.0f, 3.0f, 0.0f))*m_m4cube);
 
 	//X-3
 	cube->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-3.0f, -1.0f, 0.0f))*m_m4cube);
@@ -98,7 +123,7 @@ void Application::Display(void)
 
 		
 	fPos += 0.05f;
-
+	*/
 	// draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
 	
